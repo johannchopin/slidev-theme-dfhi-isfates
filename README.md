b3440738cccbfabddff987d3f1b9d5cb8c67da33
johannchopin <johannchopin@pm.me>
2024-07-13 13:24:26 +0200

    fix: schools asset

--- README.md
+++ README.md
@@ -24,7 +24,7 @@ A (...) theme for [Slidev](https://github.com/slidevjs/slidev).
 Add the following frontmatter to your `slides.md`. Start Slidev then it will prompt you to install the theme automatically.
 
 <pre><code>---
-theme: <b>dfhi-isfates-slidev-theme</b>
+theme: <b>slidev-theme-dfhi-isfates</b>
 ---</code></pre>
 
 Learn more about [how to use a theme](https://sli.dev/themes/use).
--- global-bottom.vue
+++ global-bottom.vue
@@ -1,8 +1,12 @@
+<script>
+import Schools from './assets/school-logos.png'
+</script>
+
 <template>
   <footer 
     class="absolute bottom-0 left-0 w-full flex justify-between items-end"
   >
     <CornerBottomLeft :slideCounter="$nav.currentLayout !== 'cover'" />
-    <img src="../assets/school-logos.png" class="w-1/5 h-full mr-2" />
+    <img :src="Schools" class="w-1/5 h-full mr-2" />
   </footer>
 </template>
